3646ddeae286222009c2a6acea987c1180132f1c
nathanallen <thenathanator@gmail.com>
2015-12-03 08:34:33 -0800

    server.js stater_code

--- server.js
+++ server.js
@@ -9,105 +9,52 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // serve static files from public folder
 app.use(express.static(__dirname + '/public'));
 
-// set view engine to hbs (handlebars)
-app.set('view engine', 'hbs');
+/************
+ * DATABASE *
+ ************/
 
-
-// pre-seeded todo data; our "database" is an array for now
+// our database is an array for now with some hardcoded values
 var todos = [
-  { _id: 1, task: 'Laundry', description: 'Wash clothes' },
-  { _id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
-  { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
+  // { _id: 1, task: 'Laundry', description: 'Wash clothes' },
+  // { _id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
+  // { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
 ];
 
+/**********
+ * ROUTES *
+ **********/
 
-// HOMEPAGE ROUTE
+/*
+ * HTML Endpoints
+ */
 
-app.get('/', function (req, res) {
+app.get('/', function homepage (req, res) {
   res.render('index');
 });
 
 
-// API ROUTES
+/*
+ * JSON API Endpoints
+ */
 
-// get all todos
-app.get('/api/todos', function (req, res) {
-  // send all todos as JSON response
-  res.json({ todos: todos });
-});
+app.get('/api/todos/search', function search(req, res){});
 
-// create new todo
-app.post('/api/todos', function (req, res) {
-  // create new todo with form data (`req.body`)
-  var newTodo = req.body;
-  
-  // set sequential id (last id in `todos` array + 1)
-  if (todos.length > 0) {
-    newTodo._id = todos[todos.length - 1]._id + 1;
-  } else {
-    newTodo._id = 1;
-  }
+app.get('/api/todos', function index(req, res) {});
 
-  // add newTodo to `todos` array
-  todos.push(newTodo);
+app.post('/api/todos', function create(req, res) {});
 
-  // send newTodo as JSON response
-  res.json(newTodo);
-});
+app.get('/api/todos/:id', function show(req, res) {});
 
-// get one todo
-app.get('/api/todos/:id', function (req, res) {
-  // get todo id from url params (`req.params`)
-  var todoId = parseInt(req.params.id);
+app.put('/api/todos/:id', function update(req, res) {});
 
-  // find todo to by its id
-  var foundTodo = todos.filter(function (todo) {
-    return todo._id == todoId;
-  })[0];
+app.delete('/api/todos/:id', function destroy(req, res) {});
 
-  // send foundTodo as JSON response
-  res.json(foundTodo);
-});
 
-// update todo
-app.put('/api/todos/:id', function (req, res) {
-  // get todo id from url params (`req.params`)
-  var todoId = parseInt(req.params.id);
-
-  // find todo to update by its id
-  var todoToUpdate = todos.filter(function (todo) {
-    return todo._id == todoId;
-  })[0];
-
-  // update the todo's task
-  todoToUpdate.task = req.body.task;
-
-  // update the todo's description
-  todoToUpdate.description = req.body.description;
-
-  // send back updated todo
-  res.json(todoToUpdate);
-});
-
-// delete todo
-app.delete('/api/todos/:id', function (req, res) {
-  // get todo id from url params (`req.params`)
-  var todoId = parseInt(req.params.id);
-
-  // find todo to delete by its id
-  var todoToDelete = todos.filter(function (todo) {
-    return todo._id == todoId;
-  })[0];
-  
-  // remove todo from `todos` array
-  todos.splice(todos.indexOf(todoToDelete), 1);
-  
-  // send back deleted todo
-  res.json(todoToDelete);
-});
-
+/**********
+ * SERVER *
+ **********/
 
 // listen on port 3000
 app.listen(3000, function() {
-  console.log('server started');
+  console.log('server running on localhost://3000');
 });
